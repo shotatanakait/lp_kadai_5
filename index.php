7a51c7c56bd72ff91bf773ff8bc1684009358f35
shotatanaka <shota9988@gmail.com>
2024-05-05 16:06:59 +0900

    ref #1 ベースとなるindex.phpを作成し,ヘッダとフッタを読込み

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dummy</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <!-- header -->
+    <?php include('header.php'); ?>
+
+    <!-- contents -->
+    <article class="container">
+        <!-- コンテンツを記述 -->
+    </article>
+
+    <!-- footer -->
+    <?php include('footer.php'); ?>
+
+    <!-- script -->
+    <script
+        src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+        crossorigin="anonymous"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
