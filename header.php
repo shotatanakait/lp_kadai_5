0f8fe5a8d37036dde6c96214908b281099e95df4
shotatanaka <shota9988@gmail.com>
2024-05-05 16:04:51 +0900

    ref #1 ヘッダを作成

---
+++ header.php
@@ -0,0 +1,28 @@
+<?php
+    $navList = [
+        'スケジュール' => './index.html#schedule',
+        'アクセス' => './index.html#access',
+        '席予約' => './index.html#reservation'
+    ];
+?>
+
+<header>
+    <div class="headerInner">
+        <div class="siteLogo">
+            <img src="img/logo.png" alt="Dummy">
+        </div>
+        <nav class="siteNav">
+            <ul class="navList">
+                <?php foreach ($navList as $key => $value) : ?>
+                    <li class="navListItem"><a href="<?php echo $value; ?>" class="navListLink"><?php echo $key; ?></a></li>
+                <?php endforeach; ?>
+            </ul>
+            <!-- humburger menu -->
+            <button class="humburgerMenuButton" id="humburgerMenuButton">
+                <span class="bar barTop"></span>
+                <span class="bar barMiddle"></span>
+                <span class="bar barBottom"></span>
+            </button>
+        </nav>
+    </div>
+</header>
