37d979ed427abefe1f8ef610cff4bc43678a0a66
shotatanaka <shota9988@gmail.com>
2024-05-06 12:20:44 +0900

    ref #1 ハンバーガーメニューのスタイル,イベントを作成

--- js/index.js
+++ js/index.js
@@ -1,4 +1,45 @@
 $(function() {
+    // HUMBURGER MENU CLICK EVENT
+    $("#humburgerMenuButton").click(function () {
+        if ($(this).hasClass('closeMenuButton')) {
+            $(this).removeClass('closeMenuButton');
+            $('.navList').slideToggle(300, function() {
+                $(this).removeAttr('style');
+            });
+        } else {
+            $(this).addClass('closeMenuButton');
+            $('.navList').slideToggle(300);
+        }
+    });
+
+    // NAVIGATION LINK CLICK EVENT
+    $('.navListItem a').click(function() {
+        if ($(window).width() <= 480) {
+            if ($('#humburgerMenuButton').hasClass('closeMenuButton')) {
+                $('#humburgerMenuButton').removeClass('closeMenuButton');
+                $('.navList').slideToggle(300, function() {
+                    $(this).removeAttr('style');
+                });
+            } else {
+                $('#humburgerMenuButton').addClass('closeMenuButton');
+                $('.navList').slideToggle(300);
+            }
+        }
+
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 300);
+    });
+
+    // WINDOW RESIZE EVENT
+    $(window).resize(function() {
+        if ($(window).width() > 480 && $('.navList').attr('style')) {
+            $('.navList').attr('style', 'display: flex !important;');
+        } else if ($(window).width() <= 480 && $('.navList').attr('style')) {
+            $('.navList').attr('style', 'display: block !important;');
+        }
+    });
+
     // SWIPER
     const swiper = new Swiper('.swiper', {
         loop: true,
