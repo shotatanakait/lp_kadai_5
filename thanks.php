bb902d19dd4f0455df59c48fe8a41119b00b703b
shotatanaka <shota9988@gmail.com>
2024-05-07 23:50:46 +0900

    ref #5 サンクスページを作成

---
+++ thanks.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dummy</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <!-- header -->
+    <?php $prefix = './index.php'; ?>
+    <?php include('header.php'); ?>
+
+    <?php
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            header('Location: index.php');
+            exit;
+        }
+    ?>
+
+    <!-- contents -->
+    <article class="container">
+        <section class="thanksSection">
+            <h1 class="thanksSectionTitle">お問い合わせありがとうございました</h1>
+        </section>
+    </article>
+
+    <!-- footer -->
+    <?php include('footer.php'); ?>
+
+    <!-- script -->
+    <script
+        src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+    <script src="js/index.js"></script>
+</body>
+</html>
